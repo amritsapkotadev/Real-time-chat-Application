59329120a66cc3391b972e68d49be8c2eedff304
Amrit Sapkota <amrit@Amrits-MacBook-Pro.local>
2025-11-27 16:24:21 +0545

    database setup

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const { chats } = require('./data');
+require('dotenv').config();      
 
- const app = express();
+const { chats } = require('./data');
+const connectdb = require('./config/db');
+const colors = require('colors');
+connectdb();                   
+
+const app = express();
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const PORT =  5000;
+const PORT = 5000;
+
 app.get("/api/chat", (req, res) => {
     res.send(chats);
 });
 
 app.get("/api/chat/:id", (req, res) => {
-    //console.log(req.params.id);
     const singleChat = chats.find((c) => c._id === req.params.id);
     res.send(singleChat);
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`.yellow.bold);
 });
