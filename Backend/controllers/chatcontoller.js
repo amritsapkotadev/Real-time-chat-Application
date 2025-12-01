585045f658ae6e76af48f61b4438f0c4b43b3e75
Amrit Sapkota <amrit@Amrits-MacBook-Pro.local>
2025-12-01 17:54:20 +0545

    Rename the group logic added and tested

--- Backend/controllers/chatcontoller.js
+++ Backend/controllers/chatcontoller.js
@@ -109,5 +109,22 @@ const createGroupChat = asyncHandler(async (req, res) => {
     }
 });
 
+const renameGroup = asyncHandler(async (req, res) => {
+    const { chatId, chatName } = req.body;
 
-module.exports={accesschat, fetchChats, createGroupChat};
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        { chatName: chatName },
+        { new: true }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+    if (!updatedChat) {
+        res.status(404);
+        throw new Error("Chat Not Found");
+    } else {
+        res.json(updatedChat);
+    }
+});
+
+module.exports={accesschat, fetchChats, createGroupChat, renameGroup};
