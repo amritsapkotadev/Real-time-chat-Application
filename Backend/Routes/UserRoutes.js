85850a83b148919d3e9d8d0baf353f2c851a3868
Amrit Sapkota <amrit@Amrits-MacBook-Pro.local>
2025-12-01 16:55:55 +0545

    user Routes added

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser,authUser } = require('../controllers/usercontroller');
-
+const { registerUser,authUser,allUsers } = require('../controllers/usercontroller');
+const { protect } = require('../middleware/Authmiddleware');    
 router.route('/register').post(registerUser);
 router.route('/login').post(authUser);
-
-
+router.route('/').get(protect, allUsers);
 
 module.exports = router;
