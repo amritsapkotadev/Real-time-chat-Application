357fd5e8d17d83d58b0957ba71a50b62b1caada6
Amrit Sapkota <amrit@Amrits-MacBook-Pro.local>
2025-12-01 16:58:41 +0545

    chat route added

---
+++ Backend/Routes/ChatRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/Authmiddleware');
+const { accesschat,fetchChats } = require('../controllers/chatcontoller');
+router.route("/").post(protect,accesschat);
+router.route("/").get(protect,fetchChats)  ;
+// router.route("/group").post(protect,createGroupChat)  ;
+// router.route("/rename").put(protect,renameGroup)  ;
+// router.route("/groupadd").put(protect,addToGroup)  ;
+// router.route("/groupremove").put(protect,removeFromGroup)  ;        
+module.exports = router;
