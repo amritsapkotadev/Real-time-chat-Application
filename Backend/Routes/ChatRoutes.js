7bbe02ebd9f699ecf788e560ba6a5d85e16a13d4
Amrit Sapkota <amrit@Amrits-MacBook-Pro.local>
2025-12-01 17:53:40 +0545

    Rename the group route added

--- Backend/Routes/ChatRoutes.js
+++ Backend/Routes/ChatRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/Authmiddleware');
-const { accesschat,fetchChats,createGroupChat } = require('../controllers/chatcontoller');
+const { accesschat,fetchChats,createGroupChat, renameGroup} = require('../controllers/chatcontoller');
 router.route("/").post(protect,accesschat);
 router.route("/").get(protect,fetchChats)  ;
 router.route("/group").post(protect,createGroupChat)  ;
-// router.route("/rename").put(protect,renameGroup)  ;
+router.route("/rename").put(protect,renameGroup)  ;
 // router.route("/groupadd").put(protect,addToGroup)  ;
 // router.route("/groupremove").put(protect,removeFromGroup)  ;        
 module.exports = router;
