409fd05d9f678496732ce9c7db05d3eacee00ab5
Amrit Sapkota <amritsapkota.dev@gmail.com>
2025-12-02 20:18:50 +0545

    feat: Add message routes with auth

--- Backend/Routes/MessageRoute.js
+++ Backend/Routes/MessageRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/Authmiddleware');
-const { sendMessage,fetchallmessages } = require('../controllers/messageController');
+const { sendMessage, fetchallmessages } = require('../controllers/messagecontroller');
 
 // logged in le matrai message pathauna ani fetch garna milcha so protect middleware use gareko
 router.route("/").post(protect, sendMessage);
