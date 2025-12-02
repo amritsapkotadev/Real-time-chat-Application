ccdcf506a24af38f56e91cfbfa3711c1c76fb481
Amrit Sapkota <amritsapkota.dev@gmail.com>
2025-12-02 17:40:40 +0545

    refactor: integrate ChatProvider into App routing structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/Loginpage";
 import SignupPage from "./pages/Signuppage";
 import Chatpage from "./pages/Chatpage";
+import ChatProvider from "./context/chatprovider";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/chats" element={<Chatpage />} />
-      </Routes>
+      <ChatProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/chats" element={<Chatpage />} />
+        </Routes>
+      </ChatProvider>
     </Router>
   );
 }
