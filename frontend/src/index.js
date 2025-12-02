7e8afff0e470285433ffa09a0ee8c9a376bb84da
Amrit Sapkota <amritsapkota.dev@gmail.com>
2025-12-02 17:41:11 +0545

    refactor: remove React.StrictMode wrapper from root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,9 @@ import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
